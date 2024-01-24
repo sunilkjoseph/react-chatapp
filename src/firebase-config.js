7fa57c82748768e2fa106fe538259e13fe36e1bb
Sunil K Joseph <104191368+sunilkjoseph@users.noreply.github.com>
2024-01-24 14:54:52 +0530

    Title Updated

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,6 @@ import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCoGIgqcv1JIeInezP49-D0Vhnd54k_n24",
-//   authDomain: "e-blah.firebaseapp.com",
-//   projectId: "e-blah",
-//   storageBucket: "e-blah.appspot.com",
-//   messagingSenderId: "104684165785",
-//   appId: "1:104684165785:web:cd42ff3217c5b892dc7608",
-//   measurementId: "G-DSD8CSKZ1M"
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyBsE9rr0_19OMsS6aoxl9_v-m-R-M4o06s",
